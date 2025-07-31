3c883a12ab99e89deb5d3d14797008a6a1478abd
RMateosAyesa <rmateos@emergya.com>
2025-07-31 22:01:58 +0200

    cosas

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,17 @@
-// playwright.config.js
 const { defineConfig } = require('@playwright/test');
-
 const isCI = !!process.env.CI;
 
 module.exports = defineConfig({
   testDir: './tests',
-  fullyParallel: false, // tests de uno en uno
+  fullyParallel: false,
+  retries: isCI ? 1 : 0, // opcional
   use: {
+    headless: isCI, // solo headless en CI
     browserName: 'chromium',
-    headless: isCI, // Headless solo en CI
-    baseURL: 'https://manager.dev.devaid.eu/', // cambia por tu URL real
+    baseURL: 'https://manager.dev.devaid.eu/',
     viewport: { width: 1280, height: 720 },
     trace: 'on-first-retry',
+    video: 'on', // activa grabación de vídeo
   },
+  reporter: [['html', { open: 'never' }]],
 });

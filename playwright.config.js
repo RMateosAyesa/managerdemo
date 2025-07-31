986ae7921d1e18b36a1db1ac500ec3bf4a15c7f4
RMateosAyesa <rmateos@emergya.com>
2025-07-31 20:23:57 +0200

    headless en CI

--- playwright.config.js
+++ playwright.config.js
@@ -1,12 +1,14 @@
 // playwright.config.js
 const { defineConfig } = require('@playwright/test');
 
+const isCI = !!process.env.CI;
+
 module.exports = defineConfig({
   testDir: './tests',
   fullyParallel: false, // tests de uno en uno
   use: {
     browserName: 'chromium',
-    headless: false,
+    headless: isCI, // Headless solo en CI
     baseURL: 'https://manager.dev.devaid.eu/', // cambia por tu URL real
     viewport: { width: 1280, height: 720 },
     trace: 'on-first-retry',
